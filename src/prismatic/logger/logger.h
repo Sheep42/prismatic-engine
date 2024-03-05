076e226f77aae25707f706d211d415c502a8beb5
Dan Shedd <dan@dshedd.com>
2024-03-05 17:10:51 -0500

    Document Logger

--- src/prismatic/logger/logger.h
+++ src/prismatic/logger/logger.h
@@ -2,11 +2,46 @@
 #define LOGGER_H
 
 typedef struct Logger {
+	// Log an informative message to the Console
+	// 
+	// ----
+	// 
+	// const char* msg
 	void ( *info )( const char* );
+
+	// Log a debug message to the Console
+	// 
+	// ----
+	// 
+	// const char* msg
 	void ( *debug )( const char* );
+
+	// Log an error message to the Console
+	// 
+	// ----
+	// 
+	// const char* msg
 	void ( *error )( const char* );
+
+	// Log an informative message to the Console with printf-style formatting
+	// 
+	// ----
+	// 
+	// const char* msg
 	void ( *infof )( const char*, ... );
+
+	// Log a debug message to the Console with printf-style formatting
+	// 
+	// ----
+	// 
+	// const char* msg
 	void ( *debugf )( const char*, ... );
+
+	// Log an error message to the Console with printf-style formatting
+	// 
+	// ----
+	// 
+	// const char* msg
 	void ( *errorf )( const char*, ... );
 } Logger;
 
