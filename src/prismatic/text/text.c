1a0c81ab31f3730b3c0ec903428da54979ff4c31
Dan Shedd <dan@dshedd.com>
2024-03-09 10:59:21 -0500

    Implement convenience string compare

--- src/prismatic/text/text.c
+++ src/prismatic/text/text.c
@@ -12,7 +12,12 @@ static void deleteString( string str ) {
 	free( str );
 }
 
+static bool stringEquals( string str1, string str2 ) {
+	return strcmp( str1, str2 ) == 0;
+}
+
 const StringUtils* prismaticString = &(StringUtils) {
 	.new = newString,
 	.delete = deleteString,
+	.equals = stringEquals,
 };
--- src/prismatic/text/text.h
+++ src/prismatic/text/text.h
@@ -2,10 +2,16 @@
 #ifndef TEXT_H
 #define TEXT_H
 
+#ifndef STDBOOL_INCLUDED
+	#define STDBOOL_INCLUDED
+	#include <stdbool.h>
+#endif
+
 typedef char* string;
 typedef struct StringUtils {
 	string ( *new )( string );
 	void ( *delete )( string );
+	bool ( *equals )( string, string );
 } StringUtils;
 
 extern const StringUtils* prismaticString;
