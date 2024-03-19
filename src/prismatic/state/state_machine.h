14e111fe554cded506a1cef629bad20e9dc92782
Dan Shedd <dan@dshedd.com>
2024-03-19 17:46:51 -0400

    Add documentation to state machine

--- src/prismatic/state/state_machine.h
+++ src/prismatic/state/state_machine.h
@@ -24,18 +24,79 @@ typedef struct StateMachine {
 } StateMachine;
 
 typedef struct StateMachineFn {
+	// Create a new State Machine
+	// 
+	// ---
+	// 
+	// State* defaultState
 	StateMachine* ( *new )( State* );
+	// Delete a State Machine
+	// 
+	// ---
+	// 
+	// StateMachine* stateMachine
 	void ( *delete )( StateMachine* );
+	// Update the State Machine
+	// 
+	// Generally you will want to call this from inside of the game's main
+	// update loop.
+	// 
+	// ---
+	// 
+	// StateMachine* stateMachine
+	// 
+	// float delta
 	void ( *update )( StateMachine*, float );
+	// Change the State Machine's current state to the named State
+	// 
+	// ---
+	// 
+	// StateMachine* stateMachine
+	// 
+	// string stateName
 	State* ( *changeStateByName )( StateMachine*, char* );
+	// Change the State Machine's current state to the provided State
+	// 
+	// ---
+	// 
+	// StateMachine* stateMachine
+	// 
+	// State* state
 	State* ( *changeState )( StateMachine*, State* );
+	// Change the State Machine's current State to its previous State
+	// 
+	// ---
+	// 
+	// StateMachine* stateMachine
 	State* ( *changeToPrevious )( StateMachine* );
+	// Change the State Machine's current State to its default State
+	// 
+	// ---
+	// 
+	// StateMachine* stateMachine
 	State* ( *changeToDefault )( StateMachine* );
+	// Add a State to the State Machine
+	// 
+	// ---
+	// 
+	// StateMachine* stateMachine
+	// 
+	// State* state
 	void ( *addState )( StateMachine*, State* );
 } StateMachineFn;
 
 typedef struct StateFn {
+	// Create a new State
+	// 
+	// ---
+	// 
+	// string stateName - A unique identifier for this State
 	State* (*new)( const char* );
+	// Delete a State
+	// 
+	// ---
+	// 
+	// State* state
 	void (*delete)( State* );
 } StateFn;
 
